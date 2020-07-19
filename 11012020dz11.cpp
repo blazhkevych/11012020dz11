364d10a42ae29e264788f8b22b52966339edc522
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:15:30 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz11.cpp
@@ -0,0 +1,17 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры три числа.Посчитать 	их сумму, произведение и среднее 	арифметическое.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double a, b, c;
+	cout << "Введите три числа : ";
+	cin >> a >> b >> c;
+	cout << "Сумма = " << a + b + c << endl;
+	cout << "Произведение = " << a * b * c << endl;
+	cout << "Среднее арифметическое = " << (a + b + c) / 3 << endl;
+}
